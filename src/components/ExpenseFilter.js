47ff6476f80c46509bfbb2fb386d864d7e0fa1e2
Moumita <dasmoumita445@gmail.com>
2021-06-29 01:11:15 +0530

    Filter expenses by year

---
+++ src/components/ExpenseFilter.js
@@ -0,0 +1,29 @@
+import { map, uniq } from 'lodash';
+import 'react-dropdown/style.css';
+const ExpenseFilter = ({ expenses, selectedYear, selectDropDownHandler }) => {
+  const years = map(expenses, (exp) =>
+    exp.date.toLocaleString('en-us', { year: 'numeric' })
+  );
+  const uniqueYears = uniq(years);
+
+  const changeDropDownHandler = (event) => {
+    selectDropDownHandler(event.target.value);
+  };
+
+  return (
+    <div className='expense-filter'>
+      <div className='expense-filter-control'>
+        <label>Filter By year</label>
+        <select value={selectedYear} onChange={changeDropDownHandler}>
+          {map(uniqueYears, (year) => {
+            return <option value={year}>{year}</option>;
+          })}
+        </select>
+      </div>
+      <div className='expense-filter-action'></div>
+    </div>
+  );
+  //   return <Dropdown options={uniqueYears} />;
+};
+
+export default ExpenseFilter;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,17 +1,39 @@
+import { useState } from 'react';
+import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
+
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState('2021');
+
+  const selectDropDownHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+    console.log(selectedYear, '*************');
+  };
+
+  const filteredExpenses = props.items.filter((exp) => {
+    console.log(exp.date.getFullYear().toString());
+    return exp.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
-    <div className='expenses'>
-      {props.items.map((item) => {
-        return (
-          <ExpenseItem
-            key={item.id}
-            date={item.date}
-            title={item.title}
-            amount={item.amount}
-          />
-        );
-      })}
+    <div>
+      <ExpenseFilter
+        selectedYear={selectedYear}
+        expenses={props.items}
+        selectDropDownHandler={selectDropDownHandler}
+      />
+      <div className='expenses'>
+        {filteredExpenses.map((item) => {
+          return (
+            <ExpenseItem
+              key={item.id}
+              date={item.date}
+              title={item.title}
+              amount={item.amount}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
